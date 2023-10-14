5d7c6d8cb6e3fd686ac6dcc642090449f27b7b5c
Vladislav Koskov <vladkoskov2005@gmail.com>
2023-10-14 14:40:57 +0700

    Simplified mini3

--- mini_tasks/mini3.py
+++ mini_tasks/mini3.py
@@ -1,15 +1,9 @@
 def make_matrice(input_string):
-    matrice = []
-    line_count = 0
-    for line in input_string.split("|"):
-        matrice.append([])
-        for num in line.split():
-            matrice[line_count].append(float(num))
-        line_count += 1
-
+    matrice = [[float(num) for num in line.split()] for line in input_string.split("|")]
     return matrice
 
 
 # test
 a = make_matrice(input())
+print(a)
 print(a[0][1])
