929fc4b2900edf660f8e5612f291cd7c5bae924d
Vladislav Koskov <vladkoskov2005@gmail.com>
2023-11-01 16:07:43 +0700

    Fix mini9

--- mini_tasks/mini9.py
+++ mini_tasks/mini9.py
@@ -1,11 +1,18 @@
 def format_table(benchmarks, algos, results):
     first_column_len = max(map(len, benchmarks + ['Benchmark']))
+
+    col_len = [max(map(len, [(algos[i])] + [(str(results[0][i]))] + [(str(results[1][i]))] )) for i in
+               range(len(algos))]
+    print(col_len)
+
     head = f"| {'Benchmark': <{first_column_len}} |"
     total_len = first_column_len + len(algos) + 2  # +2 there
 
+    i = 0
     for alg in algos:
-        head += f" {alg} |"
-        total_len += len(alg) + 2  # and there means +2 additional spaces
+        head += f" {alg: <{col_len[i]}} |"
+        total_len += col_len[i] + 2  # and there means +2 additional spaces
+        i += 1
     print(head)
 
     print(f"|{'-' * total_len}|")
@@ -13,10 +20,10 @@ def format_table(benchmarks, algos, results):
     for bench in range(len(benchmarks)):
         line = f"| {benchmarks[bench]: <{first_column_len}} |"
         for alg in range(len(results[bench])):
-            line += f" {results[bench][alg]: <{len(algos[alg])}} |"
+            line += f" {results[bench][alg]: <{col_len[alg]}} |"
         print(line)
 
 
-format_table(["the best case", "worst"],
+format_table(["the besttttttttttttttt", "the worst"],
              ["quick sort", "merge sort", "bubble sort", "super-new ultra sort"],
-             [[1.23, 1.56, 2.0, 1.0], [3.3, 2.9, 3.9, 1.1]])
+             [[1.2888888888777777777778883, 1.56, 2.0, 1.0], [3.3, 2.9, 3.9, 1.1]])
